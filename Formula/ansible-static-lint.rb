0234a676e0096974ad9f8146cad6cb56b73ad7ff
Arnaud (Arhuman) ASSAD <arhuman@gmail.com>
2026-08-27 03:29:04 +0200

    feat: add ansible-static-lint formula (v0.2.0)

---
+++ Formula/ansible-static-lint.rb
@@ -0,0 +1,34 @@
+class AnsibleStaticLint < Formula
+  desc "Fast static linter for Ansible playbooks and roles, no Python required"
+  homepage "https://github.com/arhuman/ansible-static-lint"
+  version "0.2.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/arhuman/ansible-static-lint/releases/download/v#{version}/ansible-static-lint_#{version}_darwin_arm64.tar.gz"
+      sha256 "76d590a3284584b9cb61f0b81e1387857c469862a46104f34e65179a9ccf171d"
+    else
+      url "https://github.com/arhuman/ansible-static-lint/releases/download/v#{version}/ansible-static-lint_#{version}_darwin_amd64.tar.gz"
+      sha256 "7cd2124bc25a1c223bc59b9f335a07009f5964120036488c818c753a686c298e"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/arhuman/ansible-static-lint/releases/download/v#{version}/ansible-static-lint_#{version}_linux_arm64.tar.gz"
+      sha256 "be26b75a2bec5aaf00fa66dcfd8b006215a12ca4f4e5fa2255a6a67cc773336f"
+    else
+      url "https://github.com/arhuman/ansible-static-lint/releases/download/v#{version}/ansible-static-lint_#{version}_linux_amd64.tar.gz"
+      sha256 "2910ad9cc68d04c2d015be0b7f140a619021d32c33ea9a335b2fc21d9835d52a"
+    end
+  end
+
+  def install
+    bin.install "astl"
+  end
+
+  test do
+    assert_match "v#{version}", shell_output("#{bin}/astl --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# homebrew-tap
+
+Homebrew formulae for [arhuman](https://github.com/arhuman)'s tools.
+
+## Usage
+
+```bash
+brew tap arhuman/tap
+brew install ansible-static-lint
+```
+
+## Formulae
+
+| Formula | Description |
+|---------|-------------|
+| `ansible-static-lint` | Fast static linter for Ansible playbooks and roles (`astl`), no Python required |
